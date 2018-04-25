97a3d9b273892ff8f2feedfcbaef1160b8c55eb1
Gustavo Carvalho da Silva <gustavocarvalho_ti@hotmail.com>
2018-04-25 08:00:06 -0300

    Gustavo Carvalho
    25/04/2018

--- public/js/main.js
+++ public/js/main.js
@@ -1,2 +1,15 @@
 // Nome do modulo sem dependencia, na criação não pode omitir o []
-angular.module('alurapic', ['minhasDiretivas', 'ngAnimate']);
+angular.module('alurapic', ['minhasDiretivas', 'ngAnimate', 'ngRoute'])
+    .config(function ($routeProvider, $locationProvider) {
+        // Remove o # do link partials
+        $locationProvider.html5Mode(true);
+        $routeProvider
+            .when('/fotos', {
+                templateUrl: 'partials/principal.html',
+                controller: 'FotosController'
+            })
+            .when('/fotos/new', {
+                templateUrl: 'partials/foto.html'
+            })
+            .otherwise({redirectTo: '/fotos'});
+    });
